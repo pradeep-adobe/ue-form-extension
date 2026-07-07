52239b002a2eb742a5f5797373de7c41f4b75ab7
Pradeep Sharma <pradsharma@adobe.com>
2026-07-07 11:18:28 +0530

    fix forms

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,22 @@ import { readFileSync } from 'node:fs'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
+const root = fileURLToPath(new URL('.', import.meta.url))
 const https = {
   key: readFileSync(fileURLToPath(new URL('./certs/key.pem', import.meta.url))),
   cert: readFileSync(fileURLToPath(new URL('./certs/cert.pem', import.meta.url))),
 }
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: `${root}/index.html`,
+        rail: `${root}/rail.html`,
+        dev: `${root}/dev.html`,
+      },
+    },
+  },
   server: {
     host: 'localhost',
     port: 9080,
